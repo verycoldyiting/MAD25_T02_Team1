1e822ec0d0a94bcd37d62e6c5ac965644576df87
yuhong000 <s10258441@connect.np.edu.sg>
2025-11-25 12:57:51 +0800

    added models and dependencies

--- app/src/main/java/sg/edu/np/mad/mad25_t02_team1/models/Booking.kt
+++ app/src/main/java/sg/edu/np/mad/mad25_t02_team1/models/Booking.kt
@@ -2,17 +2,28 @@ package sg.edu.np.mad.mad25_t02_team1.models
 
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.PropertyName
 
 data class Booking(
     val id: String = "",
-    val BookingDetailsAccID: DocumentReference? = null,
-    val Category: String = "",
-    val ConcertTitle: String = "",
-    val EventID: String = "",
-    val EventTime: Timestamp? = null,
-    val PaymentMethod: String = "",
-    val PurchaseTime: Timestamp? = null,
-    val Quantity: Int = 0,
-    val Section: String = "",
-    val TotalPrice: Double = 0.0
+    @get:PropertyName("BookingDetailsAccID") @set:PropertyName("BookingDetailsAccID")
+    var bookingDetailsAccID: DocumentReference? = null,
+    @get:PropertyName("Category") @set:PropertyName("Category")
+    var category: String = "",
+    @get:PropertyName("ConcertTitle") @set:PropertyName("ConcertTitle")
+    var concertTitle: String = "",
+    @get:PropertyName("EventID") @set:PropertyName("EventID")
+    var eventID: String = "",
+    @get:PropertyName("EventTime") @set:PropertyName("EventTime")
+    var eventTime: Timestamp? = null,
+    @get:PropertyName("PaymentMethod") @set:PropertyName("PaymentMethod")
+    var paymentMethod: String = "",
+    @get:PropertyName("PurchaseTime") @set:PropertyName("PurchaseTime")
+    var purchaseTime: Timestamp? = null,
+    @get:PropertyName("Quantity") @set:PropertyName("Quantity")
+    var quantity: Int = 0,
+    @get:PropertyName("Section") @set:PropertyName("Section")
+    var section: String = "",
+    @get:PropertyName("TotalPrice") @set:PropertyName("TotalPrice")
+    var totalPrice: Double = 0.0
 )
